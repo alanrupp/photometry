13b1282b8f5b0a5c4bf88ca4dbe5fc13be2f29de
Alan Rupp <alan.rupp@gmail.com>
2019-03-08 21:16:55 -0500

    fixed heatmap code

--- plot.py
+++ plot.py
@@ -6,20 +6,23 @@ import seaborn as sns
 
 parser = argparse.ArgumentParser(description='Plot data')
 parser.add_argument('file', help='data file from combine.py', type=str)
-parser.add_argument('plottype', help='plot type', type=str,\
-                    options=['line','heatmap'])
+parser.add_argument('plottype', help='plot type', type=str)
 parser.add_argument('--treatment', help='treatment name', type=str)
 args = parser.parse_args()
 
-df = pd.read_csv(args.file)
+def plot(file, type):
+    df = pd.read_csv(file)
+    if args.plottype == 'line':
+        df = df.melt()
+        sns.lineplot(x='TIMErel', y='norm', hue='sample', data=df)
+        plt.xlabel('Time (s)')
+        plt.ylabel('deltaF/F')
+        plt.show()
 
-if args.plottype == 'line':
-    sns.lineplot(x='TIMErel', y='norm', hue='sample', data=df)
-    plt.xlabel('Time (s)')
-    plt.ylabel('deltaF/F')
-    plt.show()
+    if args.plottype == 'heatmap':
+        sns.heatmap(df.set_index('TIMErel').transpose())
+        plt.show()
 
-if args.plottype == 'heatmap':
-    df = df.spread(index='sample', columns='TIMErel', values='norm')
-    sns.heatmap(df)
-    plt.show()
+# - run -----------------------------------------------------------------------
+if __name__ == '__main__':
+    plot(args.file, args.plottype)
