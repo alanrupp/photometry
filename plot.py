e7b693f19064ddd1acc2de124bcfda7d83d67a52
Alan Rupp <alan.rupp@gmail.com>
2019-09-18 16:43:26 -0400

    updated all plotting functions

--- plot.py
+++ plot.py
@@ -1,28 +1,42 @@
-import argparse
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-import seaborn as sns
+#!/usr/bin/python
 
-parser = argparse.ArgumentParser(description='Plot data')
-parser.add_argument('file', help='data file from combine.py', type=str)
-parser.add_argument('plottype', help='plot type', type=str)
-parser.add_argument('--treatment', help='treatment name', type=str)
-args = parser.parse_args()
+'''
+Plot photometry data
+'''
 
-def plot(file, type):
+def read_file(file):
     df = pd.read_csv(file)
-    if args.plottype == 'line':
-        df = df.melt()
+    if 'norm' not in df.columns:
+        df = df.melt(id_vars='TIMErel', var_name='sample', value_name='norm')
+    return df
+
+def plot(df, plottype):
+    if plottype == 'line':
         sns.lineplot(x='TIMErel', y='norm', hue='sample', data=df)
         plt.xlabel('Time (s)')
         plt.ylabel('deltaF/F')
         plt.show()
-
-    if args.plottype == 'heatmap':
-        sns.heatmap(df.set_index('TIMErel').transpose())
+    if plottype == 'heatmap':
+        df['TIMErel'] = df['TIMErel'].apply(lambda x: round(x, 2))
+        df = df.pivot(index='TIMErel', columns='sample', values='norm')
+        sns.heatmap(df.transpose(), cmap="PiYG")
+        plt.xlabel('Time (s)')
+        plt.ylabel('deltaF/F')
         plt.show()
 
 # - run -----------------------------------------------------------------------
 if __name__ == '__main__':
-    plot(args.file, args.plottype)
+    import argparse
+    import numpy as np
+    import pandas as pd
+    from matplotlib import pyplot as plt
+    import seaborn as sns
+
+    parser = argparse.ArgumentParser(description='Plot data')
+    parser.add_argument('file', help='data file from combine.py', type=str)
+    parser.add_argument('plottype', help='plot type', type=str)
+    parser.add_argument('--treatment', help='treatment name', type=str)
+    args = parser.parse_args()
+
+    df = read_file(args.file)
+    plot(df, args.plottype)
